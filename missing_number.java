1eda52b142800275cd9ce82569372920b3416319
Hemanth Kota <cs1130233@iitd.ac.in>
2019-01-18 07:30:41 -0800

    first commit

---
+++ missing_number.java
@@ -0,0 +1,40 @@
+import java.util.Scanner; 
+
+public class missing_number{
+
+     public static void main(String []args){
+         int n = -1;
+         Scanner s = new Scanner(System.in);
+         n = s.nextInt();
+         int arr[] = new int[n];         
+         for(int i = 0; i<(n-1); i++){
+             arr[i] = s.nextInt();
+             System.out.println("arr i item :" + Integer.toString(arr[i]));
+         }
+         int start = 0;
+         int end = n-2;
+         boolean found = false;
+         int mid,out=-1;
+         while(!found){            
+            mid = (start + end)/2;
+            if(mid == end){ 
+                if(arr[mid] > mid+1)
+                    out = arr[mid]-1;
+                else 
+                    out = arr[mid]+1;
+                found = true;
+            }
+            
+            if( arr[mid] > (mid+1) )
+            {
+                end = mid-1;
+            }
+            else if(arr[mid] <= (mid+1))
+            {
+                start = mid+1;
+            }
+         }
+         System.out.println("Missing number is : " + Integer.toString(out));
+        // System.out.println("Hello World");
+     }
+}
